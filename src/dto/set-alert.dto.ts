35a021426e73b474223be263e33013bea8677587
CodeAstronautic <mishrapooja8128@gmail.com>
2024-09-07 23:45:22 +0530

    feat: price alert - implemented fucntionality for price alert

--- src/dto/set-alert.dto.ts
+++ src/dto/set-alert.dto.ts
@@ -1,8 +1,33 @@
 // src/price/dto/set-alert.dto.ts
 
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsDecimal, IsEmail, IsNotEmpty } from 'class-validator';
+
+// Define an enum for the chain
+enum Chain {
+  ETH = 'eth',
+  POLYGON = 'polygon',
+}
+
 export class SetAlertDto {
-    chain: string;
-    dollar: number;
-    email: string;
-  }
-  
+  @ApiProperty({
+    description: 'The blockchain network',
+    example: 'eth',
+    enum: Chain,
+  })
+  @IsEnum(Chain, { message: 'Chain must be either "eth" or "polygon"' })
+  chain: Chain;
+
+  @ApiProperty({ description: 'The price threshold in USD', example: 2000.0 })
+  @IsDecimal()
+  @IsNotEmpty()
+  dollar: number;
+
+  @ApiProperty({
+    description: 'The email address for alerts',
+    example: 'user@example.com',
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+}
--- src/price/price.controller.ts
+++ src/price/price.controller.ts
@@ -1,7 +1,19 @@
 // src/price/price.controller.ts
-import { Controller, Get, Post, Body, Query } from '@nestjs/common';
-import { PriceService } from './price.service';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Query,
+  HttpStatus,
+  HttpException,
+} from '@nestjs/common';
+import { ApiTags, ApiResponse, ApiBody } from '@nestjs/swagger';
 
+import { PriceService } from './price.service';
+import { SetAlertDto } from 'src/dto/set-alert.dto';
+
+@ApiTags('price')
 @Controller('price')
 export class PriceController {
   constructor(private readonly priceService: PriceService) {}
@@ -12,7 +24,29 @@ export class PriceController {
   }
 
   @Post('set-alert')
-  setPriceAlert(@Body() body: { chain: string; dollar: number; email: string }) {
-    return this.priceService.setPriceAlert(body.chain, body.dollar, body.email);
+  @ApiResponse({
+    status: 201,
+    description: 'Price alert created successfully.',
+  })
+  @ApiResponse({ status: 500, description: 'Failed to create price alert.' })
+  @ApiBody({ type: SetAlertDto })
+  async setPriceAlert(@Body() setAlertDto: SetAlertDto) {
+    try {
+      const createdAlert =
+        await this.priceService.createSetPriceAlert(setAlertDto);
+      return {
+        status: HttpStatus.CREATED,
+        message: 'Price alert created successfully.',
+        data: createdAlert,
+      };
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: `Failed to create price alert: ${error}`,
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
